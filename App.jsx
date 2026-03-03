629b0912127fa7f963e9563bbfa8828d7ad335f7
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2026-03-03 18:22:34 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+import NameForm from './nameForm';
+function App() {
+  return (
+    <div>
+    <h1>Hoşgeldin</h1>
+    <NameForm/>
+    </div>
+  );
+}
+
+export default App;
