61a705d8235c0ea14e6766b5a8872b8afd536907
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2026-03-24 12:31:40 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,38 +1,45 @@
 import { useState } from 'react'
 import './App.css'
+ 
+function Profil({isim , bolum}){
+return (
+<div style={{display: "flex",
+gap:"10px",
+justifyContent: "center",
+backgroundColor:"green",
+padding:"15px",
+color:"black",
 
-function UrunKarti({urunAdi,urunFiyat,stok}){
-return(
-  <div style={{
-  gap:"10px",
-  borderRadius:"16px",
-  backgroundColor:"pink",
-  fontSize:"10px",
-  padding:"5px",
-  maxWidth:"400px",
-  width:"100%",
-  color:"midnightblue"
-  }}>
-  <h3>{urunAdi}</h3>
-  <p>Fiyat:{urunFiyat}</p>
-  <p>Stok:{stok>0 ? " Mevcut" : "Tükendi"}</p>
-  </div>
+}}>
+
+<p>İsim: {isim}</p>
+<p> Bölüm:{bolum}</p>
+</div>
 );
 }
 
+function Dersler(){
+  return(
+    <div style={{display:"flex",
+    gap:"10px", justifyContent: "center"
+    }}>
+    <h3>Dersler</h3>
+    <ul>
+      <li>JSC</li>
+      <li>Veritabanı</li>
+      <li>Grafik</li>
+      </ul>
+      </div>
+  );
+}
+
 function App(){
-return(
-  <div style={{display:"flex",
-  backgroundColor:"gray",
-  flexDirect:"column",
-  gap:"10px",
-  justifyContent:"center",
-  alignItems:"center"
-  }}>
-  <UrunKarti urunAdi="Klavye" urunFiyat={450} stok={10}/>
-  <UrunKarti urunAdi="Mouse" urunFiyat={280} stok={0}/>
-  <UrunKarti urunAdi="Monitör" urunFiyat={300} stok={5}/>
-  </div>
-);
+  return(
+    <>
+    <h3>Profil</h3>
+    <Profil isim= "Çisem" bolum="OnYuz"/>
+    <Dersler/>
+    </>
+  );
 }
 export default App;
